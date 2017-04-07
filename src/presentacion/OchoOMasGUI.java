d5de2135af7cca1780f99e48daf799c6cc6964cb
Juan Moreno <Juan.andres.208@hotmail.com>
2017-04-07 10:42:02 -0500

    ciclo3 ya con choose file choose

--- src/presentacion/OchoOMasGUI.java
+++ src/presentacion/OchoOMasGUI.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 import java.io.*;
 import java.util.*;
 
-public class OchoOMasGUI extends JFrame{
+public class OchoOMasGUI extends JFrame {
 	public static Dimension screensize = Toolkit.getDefaultToolkit().getScreenSize();
 	private JMenuBar menu;
 	private JMenu cont;
@@ -27,110 +27,134 @@ public class OchoOMasGUI extends JFrame{
 	private Color buttonLose;
 	private int xSize;
 	private int ySize;
+	private JFileChooser explorer;
+
 	public static void main(String[] args) {
 		OchoOMasGUI gui = new OchoOMasGUI();
 	}
-	private OchoOMasGUI(){
+
+	private OchoOMasGUI() {
 		super();
-		buttonWin =  Color.GREEN;
+		buttonWin = Color.GREEN;
 		buttonLose = Color.gray;
-		gamePanel= new ArrayList<>();
-		xSize=3;
-		ySize=3;
+		gamePanel = new ArrayList<>();
+		xSize = 3;
+		ySize = 3;
 		setVisible(true);
-		setSize((int)screensize.width*1/4, (int)screensize.height*1/4);
+		setSize((int) screensize.width * 1 / 4, (int) screensize.height * 1 / 4);
 		setTitle("OchoOMas");
 		prepareElementos();
 		prepareAcciones();
 	}
-	
+
 	private void prepareElementos() {
-		centre();		
+		centre();
 		prepareElementosMenu();
-		add(menu,BorderLayout.NORTH);
+		add(menu, BorderLayout.NORTH);
 		prepareElementosTablero();
-		add(board,BorderLayout.CENTER);
+		add(board, BorderLayout.CENTER);
 		refresque();
 	}
+
 	private void refresque() {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	private void prepareElementosTablero() {
 		board = new JPanel();
 		board.setLayout(new BorderLayout());
-		buttonPanel= new JPanel();
+		buttonPanel = new JPanel();
 		buttonPanel.setLayout(new GridLayout(xSize, ySize));
-		ArrayList<Integer> numbers= getRandomNumbers(xSize,ySize); 
-		while(!isAlmostOrdered(numbers)){
-			numbers=getRandomNumbers(xSize,ySize);
+		ArrayList<Integer> numbers = getRandomNumbers(xSize, ySize);
+		while (!isAlmostOrdered(numbers)) {
+			numbers = getRandomNumbers(xSize, ySize);
 		}
-		for(int i:numbers){
-			JButton temp=new JButton(Integer.toString(i));
+		for (int i : numbers) {
+			JButton temp = new JButton(Integer.toString(i));
 			temp.setBackground(buttonLose);
 			temp.setOpaque(true);
 			temp.setContentAreaFilled(true);
 			buttonPanel.add(temp);
 			gamePanel.add(temp);
-			}
+		}
 		buttons = new JPanel();
 		buttons.setLayout(new FlowLayout());
 		play = new JButton("Play !");
 		reset = new JButton("Reset");
 		buttons.add(play);
 		buttons.add(reset);
-		board.add(buttonPanel,BorderLayout.CENTER);
+		board.add(buttonPanel, BorderLayout.CENTER);
 		board.add(buttons, BorderLayout.SOUTH);
 		steps = new JLabel("Steps: Int");
 		board.add(steps, BorderLayout.NORTH);
-		
+
 	}
+
 	private boolean isAlmostOrdered(ArrayList<Integer> numbers) {
-		int j=numbers.get(0);
-		for (int i:numbers){
-			if( i-1==j){
+		int j = numbers.get(0);
+		for (int i : numbers) {
+			if (i - 1 == j) {
 				return true;
 			}
-			j=i;
+			j = i;
 		}
 		return false;
 	}
+
 	private ArrayList<Integer> getRandomNumbers(int xSize2, int ySize2) {
-		ArrayList<Integer>res =  new ArrayList<>();
+		ArrayList<Integer> res = new ArrayList<>();
 		Random inte = new Random();
-		while(res.size()!=(xSize2*ySize2)-1){
-			int a= inte.nextInt((xSize2*ySize2)-1)+1;
-			if(res.indexOf(a)<0){
+		while (res.size() != (xSize2 * ySize2) - 1) {
+			int a = inte.nextInt((xSize2 * ySize2) - 1) + 1;
+			if (res.indexOf(a) < 0) {
 				res.add(a);
 			}
 		}
 		return res;
 	}
+
 	private void prepareAcciones() {
-		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);	
+		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
 		addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent e){
+			public void windowClosing(WindowEvent e) {
 				salga();
 			}
 		});
 		exit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				salga();			
+				salga();
 			}
 		});
-		
+		open.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Seleccione();
+			}
+		});
+
 	}
-	private void salga(){
-		int option =  JOptionPane.showConfirmDialog(this, "Exit OchoOMas?", "Exit", JOptionPane.YES_NO_OPTION,
+
+	private void Seleccione() {
+		explorer = new JFileChooser();
+		explorer.setDialogTitle("Open");
+		explorer.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		if (explorer.showOpenDialog(open) == JFileChooser.APPROVE_OPTION) {
+			JOptionPane.showMessageDialog(this, 
+					"Metodo en Constrccion,pero su ruta es" + explorer.getSelectedFile().getAbsolutePath());
+		}
+	}
+
+	private void salga() {
+		int option = JOptionPane.showConfirmDialog(this, "Exit OchoOMas?", "Exit", JOptionPane.YES_NO_OPTION,
 				JOptionPane.QUESTION_MESSAGE);
-		if(option == JOptionPane.YES_OPTION){
+		if (option == JOptionPane.YES_OPTION) {
 			System.exit(0);
-		 }
+		}
 	}
-	
+
 	private void prepareElementosMenu() {
-		menu=new JMenuBar();
-		cont =new JMenu("Menu");
+		menu = new JMenuBar();
+		cont = new JMenu("Menu");
 		{
 			open = new JMenuItem("Open");
 			save = new JMenuItem("Save");
@@ -149,8 +173,9 @@ public class OchoOMasGUI extends JFrame{
 		}
 		menu.add(cont);
 	}
+
 	private void centre() {
-		setLocation((screensize.width -getSize().width)/2, (screensize.height-getSize().height)/2);		
+		setLocation((screensize.width - getSize().width) / 2, (screensize.height - getSize().height) / 2);
 	}
 
 }
