dd4cac57adf46dc7a9b97a7ff569c52b1329f707
DanBeltF <bleachstick@hotmail.com>
2017-04-19 12:26:29 -0500

    added testecases

---
+++ src/aplicacion/OchoOmasTest.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package aplicacion;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author 2104784
+ *
+ */
+public class OchoOmasTest {
+
+	/**
+	 * Test method for {@link aplicacion.OchoOmas#changeOrder(int)}.
+	 */
+	@Test
+	public void testChangeOrder() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link aplicacion.OchoOmas#reset(int, int)}.
+	 */
+	@Test
+	public void testReset() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link aplicacion.OchoOmas#getNumber(int, int)}.
+	 */
+	@Test
+	public void testGetNumber() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link aplicacion.OchoOmas#isSolved(int)}.
+	 */
+	@Test
+	public void testIsSolved() {
+		fail("Not yet implemented");
+	}
+
+}
